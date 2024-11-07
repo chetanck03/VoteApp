1a8a8aa4411eb434600d6a571c26b64471d9c772
Chetan Kumar <131525183+chetanck03@users.noreply.github.com>
2024-11-07 14:09:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,34 +1,33 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const path = require('path')
+// index.js
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
 
-const PORT = 3000
-require('dotenv').config()
+const connectDB = require('./db/connect');
+const authenticationRoute = require('./routes/authenticationRoute');
+const candidateRoutes = require('./routes/candidateRoutes');
+const voterRoutes = require('./routes/voterRoutes');
 
-const connectDB = require('./db/connect')
-const authenticationRoute = require("./routes/authenticationRoute")
-const candidateRoutes = require("./routes/candidateRoutes")
-const voterRoutes = require("./routes/voterRoutes")
+// Middleware
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
+// Routes
+app.use('/api', authenticationRoute);
+app.use('/api', candidateRoutes);
+app.use('/api', voterRoutes);
+app.use('/images', express.static(path.join(__dirname, 'votingSystem')));
 
-app.use("/api",authenticationRoute)
-app.use("/api",candidateRoutes)
-app.use("/api",voterRoutes)
-app.use('/images',express.static(path.join(__dirname,'votingSystem')))
-
-
+// Database Connection
 connectDB(process.env.MONGO_URL)
-.then(
-    ()=>{
-        console.log("Database connected")
-        app.listen(PORT,()=>{
-            console.log(`server is running on http://localhost:${PORT}/`)
-        })
-    }
-)
-.catch((error)=>{
-    console.log(error)
-})
+    .then(() => {
+        console.log('Database connected');
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+// Export app for Vercel
+module.exports = app;
